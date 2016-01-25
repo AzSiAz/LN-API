c2680be1553c0424eb89f62aa6fc7185ba93949f
AzSiAz <slider.perso@live.fr>
2016-01-25 12:28:11 +0100

    change ports

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,15 @@
+var http = 3001, https = 3002;
+if (process.env.NODE_ENV == 'test') {
+    var https = 3005
+    var http = 3006
+}
+
 module.exports = {
 	'base_url': 'http://127.0.0.1:3003',
 	'express': {
 		'adresse': '127.0.0.1',
-		'http': 3001,
-		'https': 3002
+		'http': http,
+		'https': https
 	},
     // useless for now
 	'database': {
