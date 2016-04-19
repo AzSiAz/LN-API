5b03b81b38d5a3ec357c3e4b6ef18c7fe614e7a6
AzSiAz <slider.perso@live.fr>
2016-04-19 19:00:20 +0200

    remove https config

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,10 @@
-var http = 3001, https = 3002;
-if (process.env.NODE_ENV == 'test') {
-    https = 3005
-    http = 3006
-}
+var http = 3001;
 
 module.exports = {
 	'base_url': 'http://api.azsiaz.tech:3003',
 	'express': {
 		'adresse': '127.0.0.1',
 		'http': http,
-		'https': https
 	},
     // useless for now
 	'database': {
