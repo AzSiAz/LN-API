b14a31f1281a1c5d7bc289aa3daf0a908c1d41e7
AzSiAz <slider.perso@live.fr>
2016-04-19 19:00:43 +0200

    remove https node to use reverse proxy

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const config = require('./server/config/config');
-const app = express();
 const http = require('http');
-const https = require('https');
-const fs = require("fs");
 const cors = require('cors')
-// var apicache = require('apicache').options({ debug: true }).middleware;
-
-var options = {
-  key: fs.readFileSync("key/dev.private.key"),
-  cert: fs.readFileSync("key/dev.certificate.pem")
-};
-
-// var options = {
-//   key: fs.readFileSync("key/azsiaz.tech.key"),
-//   cert: fs.readFileSync("key/azsiaz.tech.pem")
-// };
+const app = express();
 
 app.set('json spaces', 4);
 
 app.use(cors());
 
-if (process.env.NODE_ENV !== 'test') {
-    app.use(logger('dev'));
-}
+app.use(logger('dev'));
 
 app.use(bodyParser.json({
   extended: true
@@ -39,6 +24,5 @@ app.use(bodyParser.urlencoded({
 app.use('/', require('./server/routes'));
 
 var server = http.createServer(app).listen(config.express.http);
-var secureServer = https.createServer(options, app).listen(config.express.https);
 
 module.exports = app;
