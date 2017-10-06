5047e2d9754eeb7a890769943d624b4fb0bcbc5d
AzSiAz <slider.perso@live.fr>
2017-10-06 10:52:30 +0200

    change bt-api url

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const http = process.env.PORT || 3001;
 
 module.exports = {
-	base_url: 'http://btapi-shadowys.rhcloud.com',
+	base_url: 'https://btapi.herokuapp.com',
 	adresse: '0.0.0.0',
 	http: http,
 }
